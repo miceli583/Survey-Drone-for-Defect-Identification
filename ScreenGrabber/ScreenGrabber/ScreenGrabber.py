fb5f389850326a19a6a99b64f94f19a68eef5748
miceli583 <mmicel583@gmail.com>
2020-02-06 17:04:20 -0600

    Update ScreenGrabber.py

--- ScreenGrabber/ScreenGrabber/ScreenGrabber.py
+++ ScreenGrabber/ScreenGrabber/ScreenGrabber.py
@@ -1,13 +1,16 @@
 import numpy as np
 import cv2
 
+import sys
+input = sys.argv[1]
+
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 cap.set(3, 1280)
 cap.set(4, 720)
 
 ret, frame = cap.read()
 
-cv2.imwrite("image.jpg", frame)
+cv2.imwrite("%s.jpg" % input, frame)
    
 cap.release()
 cv2.destroyAllWindows()
