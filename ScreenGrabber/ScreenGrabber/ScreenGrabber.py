1b0d9e82f3d152bb90012ac4a7609ad524868881
Cailen Franze <cfranz7@lsu.edu>
2020-02-13 11:07:47 -0600

    Update with new source for Video Capture Card
    Now works with video capture card to capture a simple screen shot.

--- ScreenGrabber/ScreenGrabber/ScreenGrabber.py
+++ ScreenGrabber/ScreenGrabber/ScreenGrabber.py
@@ -1,16 +1,15 @@
 import numpy as np
 import cv2
 
-import sys
-input = sys.argv[1]
+#import sys
+#input = sys.argv[1]
 
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-cap.set(3, 1280)
-cap.set(4, 720)
+cap = cv2.VideoCapture(2)
 
 ret, frame = cap.read()
 
-cv2.imwrite("%s.jpg" % input, frame)
-   
+#cv2.imwrite("%s.jpg" % input, frame)
+cv2.imwrite("test.jpg", frame)
+ 
 cap.release()
 cv2.destroyAllWindows()
