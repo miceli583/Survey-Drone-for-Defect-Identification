360031e6ecca35e1168a544f1838cd1a3f1746f4
Cailen Franze <cfranz7@lsu.edu>
2020-02-13 11:41:39 -0600

    Random small changes. Removed test project.

--- ScreenGrabber/ScreenGrabber/ScreenGrabber.py
+++ ScreenGrabber/ScreenGrabber/ScreenGrabber.py
@@ -1,15 +1,15 @@
 import numpy as np
 import cv2
 
-#import sys
-#input = sys.argv[1]
+import sys
+input = sys.argv[1]
 
 cap = cv2.VideoCapture(2)
 
 ret, frame = cap.read()
 
-#cv2.imwrite("%s.jpg" % input, frame)
-cv2.imwrite("test.jpg", frame)
+cv2.imwrite("%s.jpg" % input, frame)
+#cv2.imwrite("test.jpg", frame)
  
 cap.release()
 cv2.destroyAllWindows()
