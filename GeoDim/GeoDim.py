1932ed7f0b28ef1e8654ac77fb6ff411e096cdf6
miceli583 <mmicel583@gmail.com>
2020-04-12 19:25:20 -0500

    Testing Data Uploaded

--- GeoDim/GeoDim.py
+++ GeoDim/GeoDim.py
@@ -1,20 +1,19 @@
 # -------GeoDim-------
 
+import time
+import math
+import csv
+import sys
 import os
 files = os.listdir('GeoDim/GeoDim/')
 
-import sys
-import csv
-
-import math
-import time
 
 def GeoDim(row):
-    
+
     lat = float(row[1])
     lon = float(row[2])
     alt = float(row[3])
-    #gimbal angle given as negative
+    # gimbal angle given as negative
     gim = float(row[4])
     dis = float(row[5])
     com = float(row[6])
@@ -27,22 +26,26 @@ def GeoDim(row):
     ymax = float(row[12])
     py = ymax-ymin
 
-    #Earth Radius = 6378100
+    # Earth Radius = 6378100
 
-    #feature altitude
+    # feature altitude
     Falt = alt - dis*math.sin(-1*gim)
-    #feature latitude
+    # feature latitude
     Flat = lat + 180*dis*math.cos(-1*gim)*math.cos(com)/(math.pi*6378100)
-    #feature longitude
+    # feature longitude
     Flon = lon + 180*dis*math.cos(-1*gim)*math.sin(com)/(math.pi*6378100)
-    #feature horizontal dimension
-    Xdim = 0.0058313294*dis*px
-    #feature vertical dimension
-    Ydim = 0.0058313294*dis*py
-    #SCALING FACTOR SHOULD BE TUNED AFTER TESTING CAMERA FOV AND RANGEFINDER
+    # feature horizontal dimension
+    #Xdim = 0.0058313294*dis*px
+    Xdim = 0.0029036507*dis*px  # oneplus
+    # feature vertical dimension
+    #Ydim = 0.0058313294*dis*py
+    Ydim = 0.0029036507*dis*py  # oneplus
+    # SCALING FACTOR SHOULD BE TUNED AFTER TESTING CAMERA FOV AND RANGEFINDER
 
-    result = [row[7],row[8],row[13],row[14],Flat,Flon,Falt,xmin,ymin,xmax,ymax,Xdim,Ydim]
+    result = [row[7], row[8], row[13], row[14], Flat,
+              Flon, Falt, xmin, ymin, xmax, ymax, Xdim, Ydim]
     return result
+
 
 data = []
 
@@ -50,17 +53,19 @@ data = []
 for entry in files:
     with open('GeoDim/GeoDim/' + entry, mode='r') as readable_file:
         next(readable_file)
-        GD_reader = csv.reader(readable_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        GD_reader = csv.reader(readable_file, delimiter=',',
+                               quotechar='"', quoting=csv.QUOTE_MINIMAL)
         for row in GD_reader:
             data.append(GeoDim(row))
 
 
+header = ['image', 'image_path', 'label', 'confidence', 'FeatureLat',
+          'FeatureLon', 'FeatureAlt', 'xmin', 'ymin', 'xmax', 'ymax', 'Xdim', 'Ydim']
 
-header = ['image','image_path','label','confidence','FeatureLat','FeatureLon','FeatureAlt','xmin','ymin','xmax','ymax','Xdim','Ydim']
+with open('%s.csv' % ('FullResults/Res_' + str(time.time())), mode='wb') as results:
+    writer = csv.writer(results, delimiter=',',
+                        quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-with open('%s.csv' %('FullResults/Res_' + str(time.time())), mode='wb') as results:
-    writer = csv.writer(results, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-  
     writer.writerow(header)
     for line in data:
         writer.writerow(line)
